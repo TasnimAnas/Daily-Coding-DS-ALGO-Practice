d3d30b84a2b5fc801e4ebd0407352ee8f112e675
Tasnimur Rahaman Anas <anasfarazi0151@gmail.com>
2021-09-04 17:38:19 +0600

    Relevant message

---
+++ DSA Revise/sept3.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+template <class anas, class rony>
+void sum(anas a, rony b)
+{
+    cout << "A+B = " << a + b;
+}
+
+
+int main()
+{
+    sum(5, 6.2);
+    return 0;
+}
